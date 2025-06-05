36b2c0694feeb62f1a2e3897f6190f46e6ec37c8
Gokul.C.Sunoj <160697089+GokulCSunoj@users.noreply.github.com>
2025-06-05 18:17:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_url_path='', static_folder='.')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:path>')
+def serve_file(path):
+    return send_from_directory('.', path)
